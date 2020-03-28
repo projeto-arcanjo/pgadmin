125beb710850e687a910870c9ce8352a050b55f1
Projeto Arcanjo <arcanjo@cmabreu.com.br>
2020-03-27 23:51:10 -0300

    Commit inicial

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM dpage/pgadmin4:4.12
+MAINTAINER magno.mabreu@gmail.com
---
+++ deploy-pgadmin4.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+mkdir /srv/pgadmin/
+
+docker ps -a | awk '{ print $1,$2 }' | grep projetoarcanjo/pgadmin | awk '{print $1 }' | xargs -I {} docker rm -f {}
+docker rmi projetoarcanjo/pgadmin
+docker build --tag=projetoarcanjo/pgadmin --rm=true .
+
+docker run --name pgadmin --hostname=pgadmin --network portico \
+-e PGADMIN_DEFAULT_EMAIL=arcanjo@cmabreu.com.br \
+-e PGADMIN_DEFAULT_PASSWORD=@rcanjo \
+-v /etc/localtime:/etc/localtime:ro \
+-p 36007:80 \
+-v /srv/pgadmin/:/var/lib/pgadmin \
+-d projetoarcanjo/pgadmin	
