0c9e1f48a29b01cc104aadb874118eb768310ac8
Projeto Arcanjo <arcanjo@cmabreu.com.br>
2020-03-28 00:34:14 -0300

    Correção no arquivo de deploy

--- deploy-pgadmin4.sh
+++ deploy-pgadmin4.sh
@@ -6,10 +6,12 @@ docker ps -a | awk '{ print $1,$2 }' | grep projetoarcanjo/pgadmin | awk '{print
 docker rmi projetoarcanjo/pgadmin
 docker build --tag=projetoarcanjo/pgadmin --rm=true .
 
-docker run --name pgadmin --hostname=pgadmin --network portico \
+docker run --name pgadmin --hostname=pgadmin  \
 -e PGADMIN_DEFAULT_EMAIL=arcanjo@cmabreu.com.br \
 -e PGADMIN_DEFAULT_PASSWORD=@rcanjo \
 -v /etc/localtime:/etc/localtime:ro \
 -p 36007:80 \
 -v /srv/pgadmin/:/var/lib/pgadmin \
 -d projetoarcanjo/pgadmin	
+
+docker network connect arcanjo pgadmin
